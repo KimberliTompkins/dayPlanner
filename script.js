5c198e163f22885fd65347f8a4cb0484edf32f83
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-04-02 18:07:55 -0600

    added save and get of local storage

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var currentHour = moment().hours();
 
 function initialize(){
     $("#currentDay").text(now);
-
+// set the colors
     for(var i = 9;i < 18;i++){
         if (i < currentHour){
             $(`#${i}`).addClass("past");  
@@ -14,13 +14,24 @@ function initialize(){
         else{
             $(`#${i}`).addClass("future");
         }
+        $(`#${i}`).text(localStorage.getItem(i));  
     }   
-        
+   //get saved event
+   
        
 }
-
-
 
 window.onload = function(){
     initialize();
    }
+
+   // Save the event
+
+   $(".saveBtn").click(function(){
+       var timeSlot =   $(this).attr("data-time")
+       console.log($(this).attr("data-time"))
+       //console.log($(`#${timeSlot}`).text)
+       localStorage.setItem($(this).attr("data-time"),$(`#${timeSlot}`).val())
+     
+   }
+   )
